d4a266d90cdc7e715edc4a072c73b5d698d17c42
Serge A. Salamanka <serge@taotesting.com>
2025-12-02 20:09:28 +0300

    fix: verbose

--- src/release.js
+++ src/release.js
@@ -262,8 +262,9 @@ export default function taoExtensionReleaseFactory(params = {}) {
         async createPullRequest() {
             log.doing('Create the pull request');
 
+            let pullRequest;
             try {
-                const pullRequest = await githubClient.createReleasePR(
+                pullRequest = await githubClient.createReleasePR(
                     data.releasingBranch,
                     params.releaseBranch,
                     data.version,
@@ -290,7 +291,7 @@ export default function taoExtensionReleaseFactory(params = {}) {
                     'There are errors on the pull request creation, things like: '
                     + 'the repository name and/or local changes that could prevent the PR '
                     + `from going through, context: ${data.package}, ${subjectType}, ${data.releasingBranch}, ${params.releaseBranch}, error: ${err}`
-                    + `full response: ${pullRequest}`
+                    + `full response: ${JSON.stringify(pullRequest, null, 2)}`
                 );
                 log.exit(
                     'Please remove the release branch remotely/locally and try again. Also '
